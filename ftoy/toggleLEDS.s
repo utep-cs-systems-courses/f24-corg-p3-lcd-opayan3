6adde32c3d736ecf0ab78413759f96007ff86251
opayan3 <opayan3@miners.utep.edu>
2024-12-12 12:31:42 -0700

    SOS

--- ftoy/toggleLEDS.s
+++ ftoy/toggleLEDS.s
@@ -1,6 +1,12 @@
-	.global toggleLEDsAsm
+	.global toggleLEDS
+	.text
+toggleLEDS:
+	;;  Toggle LED1 (P1.0)
+	BIS     #0x01, &P1OUT ; Set P1.0 to toggle (LED1 ON)
+	BIC     #0x01, &P1OUT ; Clear P1.0 to toggle (LED1 OFF)
 
-toggleLEDsAsm:
-	xor.b #0x01, &P1OUT	; Toggle LED1 (P1.0)
-	xor.b #0x40, &P1OUT	; Toggle LED2 (P1.6)
-	    reti
+	;;  Toggle LED2 (P1.6)
+	BIS     #0x40, &P1OUT ; Set P1.6 to toggle (LED2 ON)
+	BIC     #0x40, &P1OUT ; Clear P1.6 to toggle (LED2 OFF)
+
+	    RET
