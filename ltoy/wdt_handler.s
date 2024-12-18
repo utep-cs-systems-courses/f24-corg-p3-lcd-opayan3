87500e0efdc9e7d2007030bd6f04e1feaaed31a4
opayan3 <opayan3@miners.utep.edu>
2024-12-18 01:03:21 -0700

    demo 1

---
+++ ltoy/wdt_handler.s
@@ -0,0 +1,36 @@
+	.extern redrawScreen
+	.extern wdt_c_handler
+WDT:
+	;;  start of prologue
+	PUSH	R15
+	PUSH	R14
+	PUSH	R13
+	PUSH	R12
+	PUSH	R11
+	PUSH	R10
+	PUSH	R9
+	PUSH	R8
+	PUSH	R7
+	PUSH	R6
+	PUSH	R5
+	PUSH	R4
+	;;  end of prologue
+	CALL	#wdt_c_handler
+	;;  start of epilogue
+	POP	R4
+	POP	R5
+	POP	R6
+	POP	R7
+	POP	R8
+	POP	R9
+	POP	R10
+	POP	R11
+	POP	R12
+	POP	R13
+	POP	R14
+	POP	R15
+	cmp	#0, &redrawScreen
+	jz	dont_wake
+	and	#0xffef, 0(r1)	; clear CPU off in saved SR
+dont_wake:
+	RETI			;pop sr & pc
